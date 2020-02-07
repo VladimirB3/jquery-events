d1325e30c9cedc324316f0bc657e69b4be184256
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-02-07 16:07:05 +0000

    jquery events challenge completed

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $("#stream1_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream1").addClass('highlight_stream');
+   });
+   $("#stream2_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+   });
+   $("#stream3_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+   });
+
+   $(".card_para").click(function() {
+       $(".card_para").css("color", "red");
+   });
+
+   $("h2").hover(function() {
+       $("h2").css("color", "#66ccff");
+   });
+
+   $("#big_up").hover(function() {
+       $("#big_up").css("font-size", "30px");
+   });
+
+   $(".bottom_button").mouseenter(function() {
+       $("body").css("background-color", "black");
+   });
+   $(".bottom_button").mouseleave(function() {
+       $("body").css("background-color", "grey");
+   });
+}); 
